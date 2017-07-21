f0fb9de201b2a78b63acf5abf04dae59db882170
a178300 <oleksandr.chernookov@consultant.volvo.com>
2017-07-21 11:15:41 +0200

    added project sceletion

---
+++ src/main/java/RandomizedQueue.java
@@ -0,0 +1,47 @@
+import java.util.Iterator;
+
+/**
+ * Created by a178300 on 7/21/2017.
+ */
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+
+    }
+
+    // is the queue empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // return the number of items on the queue
+    public int size() {
+        return 0;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+        return null;
+    }
+
+    // return (but do not remove) a random item
+    public Item sample() {
+        return null;
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    // unit testing (optional)
+    public static void main(String[] args) {
+
+    }
+
+}
